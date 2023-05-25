0e3d582cf07fc220b94abd9558998f7988ff5b1b
Dmitriy-2022 <mitya.podmorin99@yandex.ru>
2023-05-25 23:43:36 +0300

    Review_part_1_v2.1

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,23 +1,6 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.conf import settings
-
-# from ..api.serializers import UsersSerializer
-
-# def validate_username(value):
-#     USERNAME_ME = 'Нельзя использовать "me" в качестве username'
-#     USERNAME_EMPTY = 'Поле "username" не должно быть пустым'
-#     if value == 'me' or '':
-#         invalid_username = USERNAME_ME if (
-#             value == 'me') else USERNAME_EMPTY
-#         raise serializers.ValidationError(detail=[invalid_username])
-#     result = re.findall(r'[^\w.@+-]', value)
-#     if result:
-#         raise serializers.ValidationError(
-#             f'Некорректные символы в username:'
-#             f' `{"`, `".join(set(result))}`.'
-#         )
-#     return value
+from django.contrib.auth.models import AbstractUser
 
 
 class FoodgramUser(AbstractUser):
@@ -47,6 +30,7 @@ class FoodgramUser(AbstractUser):
         verbose_name='Пароль',
         max_length=settings.LENGTH_150,
     )
+
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
