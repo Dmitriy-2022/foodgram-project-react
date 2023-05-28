09b979a80619464ee5864b3daa6ecbbac5f94336
Dmitriy-2022 <mitya.podmorin99@yandex.ru>
2023-05-28 19:57:41 +0300

    Test_v1.0

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -9,7 +9,7 @@ dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'infra', 
 load_dotenv(dotenv_path)
 
 SECRET_KEY = 'django-insecure-g3k_o9e!24q@4jkvk&#01@n8p)tm&_g!v@%2!5y99$h7&d#oc^'
-DEBUG = True
+DEBUG = False
 AUTH_USER_MODEL = 'users.FoodgramUser'
 
 ALLOWED_HOSTS = ['*']
